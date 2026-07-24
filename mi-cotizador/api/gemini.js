1f566ca41a41981c7875f3710f02a9505ced62be
Joel Ordoñez <canchondo352@gmail.com>
2026-07-24 17:25:00 -0600

    Actualizando al modelo gemini-2.5-flash

--- mi-cotizador/api/gemini.js
+++ mi-cotizador/api/gemini.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     }
 
     // 3. Conectar con Google
-    const urlGemini = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=${apiKey}`;
+    const urlGemini = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
     const response = await fetch(urlGemini, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
